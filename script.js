713fc9aec25da9c3a6170bf0a2e78a4c14f399c8
Kevin McLoughlin <kevinmcloughlin2010@gmail.com>
2015-07-14 23:12:10 -0400

    added x, o, bg color, alternation on click event

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  var tableCell = $('td');
+  var turn = 1;
+
+  tableCell.on("click", function() {
+
+    if (turn % 2 === 0) {
+      $(this).css("background-color", "coral").text('X');
+    } else {
+      $(this).css("background-color", "teal").text("O");
+    }
+
+    turn ++;
+  });
+});
